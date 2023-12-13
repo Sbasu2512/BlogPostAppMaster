e8b4c567bb8eaa68b8b9419a859f3b517fd1269f
Sayantan Basu <sayantanbasu@live.in>
2023-12-13 12:21:00 +0530

    figured out the problem with migration

--- UsersAPI/db/migrations/index.js
+++ UsersAPI/db/migrations/index.js
@@ -1,12 +1,13 @@
 import createUsersTable from "./create_users_table.js";
 import pg from 'pg';
+import 'dotenv/config';
 
 const db = new pg.Pool({
   host: process.env.DB_HOST,
     port: process.env.DB_PORT,
-    user: process.env.DB_USER|| 'sayan',
-    password: process.env.DB_PASSWORD || '1234',
-    database: process.env.DB_NAME || 'blogpostapp_users',
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
 })
 
 const runDbMigrations = async () => {
