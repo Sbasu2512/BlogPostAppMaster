16a7203f289b55d9bfb12489b44c58eea80f5c8d
Sayantan Basu <sayantanbasu@live.in>
2023-12-25 00:02:13 +0530

    API integration done for post details page

--- blogpostclient/src/components/PostDetailPage/PostDetailPage.js
+++ blogpostclient/src/components/PostDetailPage/PostDetailPage.js
@@ -1,34 +1,36 @@
-import PostFooter from "../Post/PostFooter/postfooter";
-import PostHeader from "../Post/PostHeader/PostHeader";
+import { useParams } from "react-router-dom";
+import { useSelector } from "react-redux";
+
 
 export default function PostDetailPage(props) {
-
+   
     const {body,title,creator,time} = props;
+
+    const allPosts =  useSelector((state) => state?.posts?.AllPosts);
+
+    const post_id = useParams();
+
+    console.log(post_id.id)
+    
+    const post = allPosts.filter((x)=> x.id === post_id.id)[0];
+    
+    console.log(post)
 
     return(
         <>
         <div className="container grid grid-cols-5 gap-4 h-screen">
         <div className="col-span-1"></div>
         <div className="col-span-2 flex flex-col content-center flex-wrap h-screen">
-          <div className="overflow-auto min-h-[80%] max-h-[100%] h-[50%]">
+          <div className="overflow-auto min-h-[100%] max-h-[100%] h-[100%]">
           <div className="h-15 rounded mt-3 light-orange font-mono">
-            <span className="ml-2">{title}</span>
+            <span className="ml-2">{post.post_title}</span>
             <span className="float-right flex">
-              <p className="mr-3">{creator}</p>
-              <p className="mr-2">{time}</p>
+              <p className="mr-3">{post.user_id}</p>
+              <p className="mr-2">{post.createdon}</p>
             </span>
             <br />
             <div className="ml-2 mr-2">
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-  industry. Lorem Ipsum has been the industry's standard dummy text
-  ever since the 1500s, when an unknown printer took a galley of
-  type and scrambled it to make a type specimen book. It has
-  survived not only five centuries, but also the leap into
-  electronic typesetting, remaining essentially unchanged. It was
-  popularised in the 1960s with the release of Letraset sheets
-  containing Lorem Ipsum passages, and more recently with desktop
-  publishing software like Aldus PageMaker including versions of
-  Lorem Ipsum.
+           {post.post_body}
             </div>
             <div className="flex flex-row bg-yellow-200">
               <span className="w-[50%] flex content-center justify-center justify-items-center">
@@ -40,6 +42,7 @@ export default function PostDetailPage(props) {
                 >
                   <path d="M7.493 18.5c-.425 0-.82-.236-.975-.632A7.48 7.48 0 0 1 6 15.125c0-1.75.599-3.358 1.602-4.634.151-.192.373-.309.6-.397.473-.183.89-.514 1.212-.924a9.042 9.042 0 0 1 2.861-2.4c.723-.384 1.35-.956 1.653-1.715a4.498 4.498 0 0 0 .322-1.672V2.75A.75.75 0 0 1 15 2a2.25 2.25 0 0 1 2.25 2.25c0 1.152-.26 2.243-.723 3.218-.266.558.107 1.282.725 1.282h3.126c1.026 0 1.945.694 2.054 1.715.045.422.068.85.068 1.285a11.95 11.95 0 0 1-2.649 7.521c-.388.482-.987.729-1.605.729H14.23c-.483 0-.964-.078-1.423-.23l-3.114-1.04a4.501 4.501 0 0 0-1.423-.23h-.777ZM2.331 10.727a11.969 11.969 0 0 0-.831 4.398 12 12 0 0 0 .52 3.507C2.28 19.482 3.105 20 3.994 20H4.9c.445 0 .72-.498.523-.898a8.963 8.963 0 0 1-.924-3.977c0-1.708.476-3.305 1.302-4.666.245-.403-.028-.959-.5-.959H4.25c-.832 0-1.612.453-1.918 1.227Z" />
                 </svg>
+                {post.likescount === null? 0: post.likescount}
               </span>
               <span className="w-[50%] flex content-center justify-center justify-items-center">
                 <svg
@@ -50,6 +53,7 @@ export default function PostDetailPage(props) {
                 >
                   <path d="M15.73 5.5h1.035A7.465 7.465 0 0 1 18 9.625a7.465 7.465 0 0 1-1.235 4.125h-.148c-.806 0-1.534.446-2.031 1.08a9.04 9.04 0 0 1-2.861 2.4c-.723.384-1.35.956-1.653 1.715a4.499 4.499 0 0 0-.322 1.672v.633A.75.75 0 0 1 9 22a2.25 2.25 0 0 1-2.25-2.25c0-1.152.26-2.243.723-3.218.266-.558-.107-1.282-.725-1.282H3.622c-1.026 0-1.945-.694-2.054-1.715A12.137 12.137 0 0 1 1.5 12.25c0-2.848.992-5.464 2.649-7.521C4.537 4.247 5.136 4 5.754 4H9.77a4.5 4.5 0 0 1 1.423.23l3.114 1.04a4.5 4.5 0 0 0 1.423.23ZM21.669 14.023c.536-1.362.831-2.845.831-4.398 0-1.22-.182-2.398-.52-3.507-.26-.85-1.084-1.368-1.973-1.368H19.1c-.445 0-.72.498-.523.898.591 1.2.924 2.55.924 3.977a8.958 8.958 0 0 1-1.302 4.666c-.245.403.028.959.5.959h1.053c.832 0 1.612-.453 1.918-1.227Z" />
                 </svg>
+             {post.dislikescount === null? 0: post.dislikescount}
               </span>
             </div>
           </div>
