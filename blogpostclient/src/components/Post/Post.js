911c6deb1d8ab8cbd40ddb246031c6a36502cc85
Sayantan Basu <sayantanbasu@live.in>
2023-12-25 23:42:42 +0530

    minor refactor

--- blogpostclient/src/components/Post/Post.js
+++ blogpostclient/src/components/Post/Post.js
@@ -1,44 +1,13 @@
-import { React, useEffect } from "react";
-import PostHeader from "./PostHeader/PostHeader";
+import { React } from "react";
 import PostBody from "./PostBody/postbody";
-import PostFooter from "./PostFooter/postfooter";
-import { Navigate, useLocation, useNavigate } from "react-router-dom";
-import { useState } from "react";
-
+import { useLocation } from "react-router-dom";
 import { useSelector } from "react-redux";
-import { ToastContainer, toast } from 'react-toastify';
-
 
 export default function Posts() {
-  const userDetails = useSelector((state) => state?.users?.userDetails);
   const allPosts = useSelector((state)=> state?.posts?.AllPosts);
   const userPosts = useSelector((state)=> state?.posts?.userPosts);
-  const [initialFetch, setInitialFetch] = useState(true)
  
-  console.log('userPosts',userPosts);
-  console.log('allPosts',allPosts);
-  
-  useEffect(()=>{
-    if(initialFetch){
-      toast.success(`Welcome ${userDetails.displayName}`, {
-        position: "top-right",
-        autoClose: 5000,
-        hideProgressBar: false,
-        closeOnClick: true,
-        pauseOnHover: true,
-        draggable: true,
-        progress: undefined,
-        theme: "colored",
-      });
-    }
-
-    return ()=>{setInitialFetch(false)}
-  },[initialFetch]);
-
-
   let token = "";
-
- 
 
   const location = useLocation();
   if (location?.state) {
@@ -47,7 +16,6 @@ export default function Posts() {
  
   return (
     <>
-    <ToastContainer />
     <div className="container">
       <PostBody token={token} allPosts={allPosts} userPosts={userPosts} />
     </div>
